49a62425314f288a5d8fded5291af58ab60656c6
nhlchrs <nhlchaurasia1234@gmail.com>
2026-01-10 07:43:03 +0530

    changes for fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import path from "path";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
